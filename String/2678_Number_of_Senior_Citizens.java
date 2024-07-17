8f567b175269081183ed3a588e225ee981a0d66a
Yashi <srivastavayashi494@gmail.com>
2024-07-17 17:50:14 +0530

    Problem 2678

---
+++ String/2678_Number_of_Senior_Citizens.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countSeniors(String[] details) {
+         int count=0;
+        for(String str:details){
+           int age=Integer.parseInt(str.substring(11,13));
+           if(age>60){
+            count++;
+           }
+        }
+        return count;
+
+        
+    }
+}
