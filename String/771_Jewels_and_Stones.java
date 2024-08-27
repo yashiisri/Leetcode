b0f5deff322e4be3070e11f54894d4f31c5b59c1
Yashi <srivastavayashi494@gmail.com>
2024-08-27 23:47:46 +0530

    Problem 771

---
+++ String/771_Jewels_and_Stones.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int sum=0;
+        int[] arr=new int[58];
+        for(char ch:jewels.toCharArray()){
+            arr[ch-'A']=1;
+        }
+        for(char ch:stones.toCharArray()){
+            sum+=arr[ch-'A'];
+        }
+        return sum;
+    }
+}
