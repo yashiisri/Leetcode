65358aa36ffe1c83de1b1f5a2320076e4c86fa4f
Yashi <srivastavayashi494@gmail.com>
2024-08-30 21:43:10 +0530

    Problem 2427

---
+++ Maths/2427_Number_of_Common_Factors.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int commonFactors(int a, int b) {
+        int s = 0;
+        int n = Math.max(a, b);
+        for (int i = 1; i <= n; i++) {
+            if (a % i == 0 && b % i == 0)
+                s++;
+        }
+        return s;
+    }
+}
