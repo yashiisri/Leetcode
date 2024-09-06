f2f28ebeb07b789ad5c7dc9eff5826b66f40dd09
Yashi <srivastavayashi494@gmail.com>
2024-09-06 19:17:52 +0530

    Problem 234

---
+++ LinkedList/234_Palindrome_Linked_List.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head){
+        if(head==null){
+            return head;
+        }
+        ListNode prev=null;
+        ListNode present=head;
+        ListNode next=present.next;
+
+        while(present!=null){
+            present.next=prev;
+            prev=present;
+            present=next;
+            if(next!=null){
+                next=next.next;
+            }
+        }
+        return prev;
+    }
+    public ListNode middleNode(ListNode head){
+        ListNode s=head;
+        ListNode f=head;
+
+        while(f!=null && f.next!=null){
+            s=s.next;
+            f=f.next.next;
+        }
+        return s;
+    }
+     
+      public boolean isPalindrome(ListNode head) {
+        ListNode mid=middleNode(head);
+        ListNode headSecond=reverseList(mid);
+        ListNode reverseHead=headSecond;
+        
+
+        while(head!=null && headSecond!=null){
+            if(head.val!=headSecond.val){
+               reverseList(reverseHead);
+                return false;
+            }
+            head=head.next;
+        headSecond=headSecond.next;
+        }
+        
+    
+     reverseList(reverseHead);
+     return true;
+      }
+}
--- LinkedList/92_Reverse_Linked_List_II.java
+++ LinkedList/92_Reverse_Linked_List_II.java
@@ -8,7 +8,7 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
+ class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
         if(left==right){
             return head;
