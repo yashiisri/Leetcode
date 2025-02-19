92f5abb864f1eb586b9c6331a732bc258fed0c30
Yashi <srivastavayashi494@gmail.com>
2025-02-19 19:46:24 +0530

    Problem 540

---
+++ Array/540_Single_Element_in_a_Sorted_Array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int ans=0;
+        for(int num:nums){
+            ans^=num;
+        }
+        return ans;
+      
+        
+    }
+}
