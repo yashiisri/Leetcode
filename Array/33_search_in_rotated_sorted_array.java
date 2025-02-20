7ad4a0cb73569f62b2332b24463728bbc6cbb39d
Yashi <srivastavayashi494@gmail.com>
2025-02-20 13:49:34 +0530

    Updated code

--- Array/33_search_in_rotated_sorted_array.java
+++ Array/33_search_in_rotated_sorted_array.java
@@ -1,51 +1,30 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int pivot = findPivot(nums, 0, nums.length - 1);
+        int start=0;
+        int end=nums.length-1;
 
-        if (pivot == -1) {
-            return binarysearch(nums, target, 0, nums.length - 1);
-        }
-        if (nums[pivot] == target) {
-            return pivot;
-        }
-
-        if (target >= nums[0]) {
-            return binarysearch(nums, target, 0, pivot - 1);
-        }
-        return binarysearch(nums, target, pivot + 1, nums.length - 1);
-    }
-
-    static int binarysearch(int[] arr, int target, int start, int end) {
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            if (target < arr[mid]) {
-                end = mid - 1;
-            } else if (target > arr[mid]) {
-                start = mid + 1;
-            } else {
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(target==nums[mid]){
                 return mid;
             }
-        }
-        return -1; // Return -1 if the target is not found
-    }
-
-    int findPivot(int[] arr, int start, int end) {
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            if (mid < end && arr[mid] > arr[mid + 1]) {
-                return mid;
-            } else if (mid > start && arr[mid] < arr[mid - 1]) {
-                return mid - 1;
-            } else {
-                if (arr[mid] <= arr[start]) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
+            if(start<=mid){
+                if(target>=nums[start] && target<nums[mid]){
+                    end=mid-1;
+                }else{
+                    start=mid+1;
                 }
             }
+                else{
+                    if(target>=nums[mid] && target<=nums[end]){
+                        start=mid+1;
+                    }else{
+                        end=mid-1;
+                    }
+                }
+            }
+            return -1;
         }
-        return -1; // Return -1 if no pivot is found
-    }
+
+    
 }
