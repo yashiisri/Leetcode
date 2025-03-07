6fafaf91e016e90352340b932dbd4689997e7db3
Yashi <srivastavayashi494@gmail.com>
2025-03-07 20:09:38 +0530

    Problem 2523

--- Numbers/2965_Find_Missing_and_Repeated_Values.java
+++ Numbers/2965_Find_Missing_and_Repeated_Values.java
@@ -1,15 +1,14 @@
-// x ^ 0 = x; x ^ x = 0; x ^ y = y ^ x;
-// x ^ y = w; w ^ x = y; w ^ y = x;
+
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
         int n = grid.length;
         boolean[] visited = new boolean[n * n + 1];
         int repeatedValue = -1;
-        // xor of all grid elements(b is missing, a will be cancel)
+
         int xorWithoutAB = 0; 
-        // xor of (1..n^2)
+       
         int xorOfAll = 0;
-        // counter (1..n^2)
+   
         int cnt = 1;
 
         for (var row : grid) {
@@ -27,8 +26,7 @@ class Solution {
             }
         }
 
-        // a ^ b = xorWithoutAB ^ xorOfAll
-        // b = a ^ b ^ repreatedValue
+     
         int missingValue = xorWithoutAB ^ xorOfAll ^ repeatedValue;
         return new int[] {repeatedValue, missingValue};
     }
